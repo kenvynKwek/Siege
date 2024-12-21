cf7901d9655435c87e0cf75b250d5ca8f75e17f3
Kenvyn Kwek <kenvynkwek@gmail.com>
2024-12-21 21:28:55 +0800

    Add 'Projectile' script and 'PlayerShoot' script
    Added 'Projectile' script to handle projectile speed and collision events. Added 'PlayerShoot' script to shoot projectiles.

---
+++ Assets/Scripts/Player Scripts/PlayerShootScript.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+// automatically shoots projectiles from attached game object center to targets in a radius
+
+public class PlayerShootScript : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float fireTimer = 0f;
+    public float fireRate = 0.8f;
+    public float shootingRange = 0.6f;
+    public float rotationOffset = -90f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireTimer += Time.deltaTime;
+
+        if (fireTimer >= fireRate)
+        {
+            GameObject target = FindNearestEnemy();
+            if (target != null) Shoot(target);
+
+            fireTimer = 0f;
+        }
+    }
+
+    /// <summary>
+    /// Shoots a projectile towards a given target.
+    /// </summary>
+    /// <param name="target">The game object to shoot at.</param>
+    void Shoot(GameObject target)
+    {
+        // get direction to target
+        Vector2 direction = (target.transform.position - transform.position).normalized;
+
+        // rotate to face enemy (note to self: may have to offset rotation because sprite)
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + rotationOffset;
+
+        // instantiate projectile
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, angle));
+
+        // access projectile attributes
+        ProjectileScript projectileScript = projectile.GetComponent<ProjectileScript>();
+        float projectileSpeed = projectileScript.projectileSpeed;
+
+        // set velocity
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+        rb.velocity = direction * projectileSpeed;
+
+        // ignore collision w/ pl○ayer
+        Collider2D playerCollider = GetComponent<Collider2D>();
+        Collider2D projectileCollider = projectile.GetComponent<Collider2D>();
+        Physics2D.IgnoreCollision(playerCollider, projectileCollider);
+    }
+
+    /// <summary>
+    /// Finds the nearest enemy to the player game object.
+    /// </summary>
+    /// <returns>The nearest enemy as a game object.</returns>
+    GameObject FindNearestEnemy()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (enemies.Length == 0) return null;
+
+        GameObject nearestEnemy = null;
+        float shortestDistance = Mathf.Infinity;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distance = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distance < shortestDistance && distance <= shootingRange)
+            {
+                shortestDistance = distance;
+                nearestEnemy = enemy;
+            }
+        }
+
+        return nearestEnemy;
+    }
+}
