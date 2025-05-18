264be90ee77eb011caf2a7b60a0eb5ea996e6253
Kenvyn Kwek <kenvynkwek@gmail.com>
2025-05-18 17:56:27 +0800

    implement CameraShake script

--- Assets/Scripts/Camera/CameraShake.cs
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -1,18 +1,63 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
+    private Vector3 originalPos;
+
+    public static CameraShake Instance;
+
+    void Awake()
+    {
+        Instance = this; // singleton pattern
+        originalPos = transform.localPosition;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    /// <summary>
+    /// Shakes the camera with a specified duration and intensity.
+    /// </summary>
+    /// <param name="duration">How long the shake lasts for.</param>
+    /// <param name="magnitude">How intense the shake is.</param>
+    /// <returns>An IEnumerator for coroutine timing.</returns>
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = originalPos + new Vector3(x, y, 0f); // shake screen
+
+            elapsedTime += Time.deltaTime; // update elapsed time
+            yield return null;
+        }
+
+        // after shake duration
+        transform.localPosition = originalPos; // reset camera offset
+    }
+
+
+    /// <summary>
+    /// Triggers a camera shake effect with a specified duration and intensity.
+    /// </summary>
+    /// <param name="duration">How long the shake lasts for.</param>
+    /// <param name="magnitude">How intense the shake is.</param>
+    public void ShakeCamera(float duration, float magnitude)
+    {
+        StartCoroutine(Shake(duration, magnitude));
     }
 }
